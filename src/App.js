6f68e1266fd2dd7293374246aa674379e357bef3
Dexter <denus.denisovuch@gmail.com>
2020-11-05 23:55:17 +0200

    Version 1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,246 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Button from '@material-ui/core/Button'
+import {
+  AppBar,CardMedia,Card, Box, Container, IconButton, Paper, Toolbar, Typography, Grid, 
+  CardContent, CardActions, Dialog, DialogTitle, DialogContent, DialogActions,
+  DialogContentText, TextField, BottomNavigation, BottomNavigationAction,
+} from '@material-ui/core';
+
+
+import  FolderIcon from '@material-ui/icons/Folder';
+import  RestoreIcon from '@material-ui/icons/Restore';
+import  FavoriteIcon from '@material-ui/icons/Favorite';
+import  LocationOnIcon from '@material-ui/icons/LocationOn';
+import  MenuIcon from '@material-ui/icons/Menu';
+import  LayerIcon from '@material-ui/icons/Layers';
+import  PlayCircleFilledIcon from '@material-ui/icons/PlayCircleFilled';
+
+import {makeStyles} from '@material-ui/core/styles';
+
+
+
+
+const useStyles = makeStyles((theme => ({
+  root:{
+    flexGrow: 1,
+  },
+  menuButton:{
+    marginRight: theme.spacing(1),
+  },
+  title:{
+    flexGrow: 1,
+  },
+  mainFeaturesPost:{
+    position: "relative",
+    color: theme.palette.common.white,    
+    marginBotom: theme.spacing(4),
+
+    backgroundSize:"cover",
+    backgroundRepeat: "no-repeat",
+    backgroundPosition: "center",
+  },   
+  mainFeaturesPostContent:{
+    position: "relative",
+    padding: theme.spacing(6),
+    marginTop: theme.spacing(8),
+  },
+  cardMedia:{
+    paddingTop:"56.25%"
+  },
+  cardContent:{
+    flexGrow:1
+  },
+  cardGrid:{
+    marginTop: theme.spacing(4)
+  },
+  
+})));
+
+
+const cards = [1,2,3,4,5,6,7,8,9];
+
+
 
 function App() {
+  const classes = useStyles();
+
+  const [value, setValue] = React.useState("recents");
+  const handleChange = (event, newValue) => {
+    setValue(newValue)
+  }
+  const [open, setOpen] = React.useState(false);
+  const handleClickOpen = () => {
+    setOpen(true)
+  } 
+  const handleClose = () => {
+    setOpen(false)
+  } 
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <AppBar position="fixed">
+      <Container fixed>
+        <Toolbar>
+          <IconButton edge="start" color="inherit" aria-label="menu" className={classes.menuButton}>
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" className={classes.title}>Web Developer Blog</Typography>
+          <Box mr={3} >
+            <Button color="inherit" variant="outlined" onClick={handleClickOpen}>Log in</Button>
+
+            <Dialog open={open} onClose={handleClose} aria-labelledby="form-dialof-title">
+              <DialogTitle id='form-dialof-title'>Log in</DialogTitle> 
+              <DialogContent>
+                <DialogContentText>Log in to see videos</DialogContentText>  
+                <TextField 
+                  autoFocus
+                  margin="dense" 
+                  id="name"
+                  label="Email Adress"
+                  type="email"
+                  fullWidth
+                />              
+                <TextField 
+                  autoFocus
+                  margin="dense" 
+                  id="pass"
+                  label="Password"
+                  type="password"
+                  fullWidth
+                />              
+              </DialogContent>  
+              <DialogActions>
+                <Button onClick={handleClose} color="primary">Cancel</Button>
+                <Button onClick={handleClose} color="primary">Log in</Button>  
+              </DialogActions>           
+            </Dialog>
+          </Box>
+          <Button color="secondary" variant="contained">Sign up</Button>
+        </Toolbar>       
+      </Container>      
+    </AppBar> 
+
+    <main>
+      <Paper 
+        className={classes.mainFeaturesPost}
+        style={{backgroundImage:`url(https://source.unsplash.com/random)`}}
+      >
+        <Container fixed>       
+          <Grid container>
+            <Grid item md={6}>
+              <div className={classes.mainFeaturesPostContent}>
+                <Typography
+                  component="h1"
+                  variant="h3"
+                  color="inherit"
+                  gutterBottom
+                >
+                  Web Developer Blog
+                </Typography>
+                <Typography
+                  component="h5"
+                  color="inherit"
+                  paragraph
+                >
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                </Typography>
+                <Button variant="contained" color="secondary">
+                  Learn more
+                </Button>
+              </div>
+            </Grid>
+          </Grid>
+        </Container>
+      </Paper>
+      <div className={classes.mainContent}>
+        <Container maxWidth="md">
+          <Typography variant="h2" align="center" color="textPrimary" gutterBottom>
+            Web Developer Blog
+          </Typography>
+          <Typography variant="h5" align="center" color="textSecondary" paragraph>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          </Typography>
+        </Container>
+      </div>
+      <div className={classes.mainButtons}>
+        <Grid container spacing={5} justify="center">
+          <Grid item>
+            <Button variant="contained" color="primary">Start now</Button>          
+          </Grid>
+          <Grid item>
+            <Button variant="outlined" color="primary">Learn more</Button>          
+          </Grid>
+        </Grid>
+      </div>
+      <Container className={classes.cardGrid} maxWidth="md">
+        <Grid container spacing={5}>
+          {cards.map((card)=>(
+            <Grid item key={card} xs={12} sm={6} md={4}>
+              <Card className={classes.card}>
+                <CardMedia 
+                className={classes.cardMedia}
+                image = 'https://source.unsplash.com/random'
+                title = 'image title'
+                />
+                  <CardContent className={classes.cardcontent}>
+                    <Typography variant="h5" gutterBottom>
+                         Blog post
+                    </Typography>                   
+                    <Typography>
+                        Blog post. Web developer blog web developer blog. Blog post
+                    </Typography>   
+                  </CardContent>                                 
+                  <CardActions>
+                    <Button size="small" color='primary'>
+                    View
+                    </Button>
+                    <Button size="small" color='primary'>
+                    Edit
+                    </Button>
+                    <LayerIcon />
+                    <PlayCircleFilledIcon />             
+                  </CardActions>
+              </Card>              
+            </Grid>
+          ))}
+        </Grid>        
+      </Container>
+    </main>
+    <footer>
+      <Typography variant="h5" align="center"  gutterBottom>Footer</Typography>
+      <BottomNavigation
+        value={value}
+        onChange={handleChange}
+        className={classes.root}
+      >
+        <BottomNavigationAction 
+          label="Recents"
+          value="recents"
+          icon={<RestoreIcon />}
+        />
+        <BottomNavigationAction 
+          label="Favorites"
+          value="favorites"
+          icon={<FavoriteIcon />}
+        />
+        <BottomNavigationAction 
+          label="Nearby"
+          value="nearby"
+          icon={<LocationOnIcon />}
+        />
+        <BottomNavigationAction 
+          label="Folder"
+          value="folder"
+          icon={<FolderIcon />}
+        />
+      </BottomNavigation>
+      <Typography align="center" color="textSecondary" component="p" variant="subtitle1"> 
+        Web Developer Blog React Js Material Ui Site
+      </Typography>
+    </footer>
+
+
+    </>
   );
 }
 
